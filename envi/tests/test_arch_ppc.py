74fa0141bfad3365df0270fb07ceea822753f1d7
Aaron Cornelius <a.aaron.cornelius@gmail.com>
2019-05-13 10:32:15 -0400

    forgot to fix the referenced module name in ppc tests

--- envi/tests/test_arch_ppc.py
+++ envi/tests/test_arch_ppc.py
@@ -30,12 +30,12 @@ class PpcInstructionSet(unittest.TestCase):
         va = 0x00000000
         vw.addMemoryMap(va, 7, 'firmware', '\xff' * 16384*1024)
 
-        import ppc64_gcc_instructions
-        for test_bytes, result_instr in ppc64_gcc_instructions.instructions:
+        import ppc64_instructions
+        for test_bytes, result_instr in ppc64_instructions.instructions:
             op = vw.arch.archParseOpcode(test_bytes.decode('hex'), 0, va)
             op_str = repr(op).strip()
             if op_str == result_instr:
                 test_pass += 1
             self.assertEqual(result_instr, op_str, '{}: {} != {}'.format(test_bytes, result_instr, op_str))
 
-        self.assertEqual(test_pass, len(ppc64_gcc_instructions.instructions))
+        self.assertEqual(test_pass, len(ppc64_instructions.instructions))
