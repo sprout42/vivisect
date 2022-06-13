1ce979ad30f09f5b34443ea5336adb8cbd9a88ab
Erin Cornelius <a.aaron.cornelius@gmail.com>
2022-06-13 14:38:15 -0400

    some minor ppc register cleanup

--- envi/archs/ppc/regs.py
+++ envi/archs/ppc/regs.py
@@ -6,19 +6,6 @@ import envi.registers as e_reg
 
 gprs32 = [('r%s' % x, 32)  for x in range(32)]
 gprs64 = [('r%s' % x, 64)  for x in range(32)]
-floats = [('f%s' % x, 64)  for x in range(32)]
-floats.append( ('FPSCR', 64) )
-vectors = [('vr%s' % x, 128)  for x in range(64)]
-vectors.append( ('VSCR', 32) )
-
-sysregs = (
-        ('acc', 64),
-        ('cr', 64),
-        ('msr', 64),
-        ('ten', 64),
-        ('SPEFSCR', 64),
-        ('pc', 64),
-        )
 
 ppc_regs32 = []
 ppc_regs = ppc_regs64 = []
@@ -26,17 +13,48 @@ ppc_regs = ppc_regs64 = []
 ppc_regs64.extend(gprs64)
 ppc_regs32.extend(gprs32)
 
+
+# Core system registers that are not also SPRs or part of another grouping of
+# registers
+sysregs = (
+    ('pc', 64),
+    ('msr', 64),
+    ('cr', 64),
+)
+
 REG_OFFSET_SYSREGS = len(ppc_regs)
 ppc_regs64.extend(sysregs)
 ppc_regs32.extend(sysregs)
 
+
+# Float registers
+float_regs = [('f%s' % x, 64)  for x in range(32)]
+float_regs.append( ('FPSCR', 64) )
+
 REG_OFFSET_FLOAT = len(ppc_regs)
-ppc_regs64.extend(floats)
-ppc_regs32.extend(floats)
+ppc_regs64.extend(float_regs)
+ppc_regs32.extend(float_regs)
+
+
+# Vector registers
+vector_regs = [('vr%d' % x, 128)  for x in range(64)]
+vector_regs.append( ('VSCR', 32) )
 
 REG_OFFSET_VECTOR = len(ppc_regs)
-ppc_regs64.extend(vectors)
-ppc_regs32.extend(vectors)
+ppc_regs64.extend(vector_regs)
+ppc_regs32.extend(vector_regs)
+
+
+# For Embedded/SP/SPE instructions there is a new ACC register that is not an
+# SPR, this is the only new SPE non-SPR/GPR register
+spe_regs = (
+    ('acc', 64),
+)
+
+REG_OFFSET_EMBEDDED = len(ppc_regs)
+ppc_regs64.extend(spe_regs)
+ppc_regs32.extend(spe_regs)
+
 
 from . import spr
 # populate spr_regs from the PPC SPR register list (in spr.py)
@@ -50,50 +68,88 @@ REG_OFFSET_SPR = len(ppc_regs)
 ppc_regs64.extend(spr_regs)
 ppc_regs32.extend(spr_regs)
 
-# sparse definition of TMR regs, so we fill in the gaps a bit
-tmr_regs = [("TMRREG%d" % x, 64) for x in range(192) ]
-tmr_regs[16] = ('tmcfg0', 64)
 
-tpri_regs = [('tpri%d' % x, 64) for x in range(32)]
-imsr_regs = [('imsr%d' % x, 64) for x in range(32)]
-inia_regs = [('inia%d' % x, 64) for x in range(32)]
-tmr_regs.extend(tpri_regs)
-tmr_regs.extend([("TPRIREG%d" % x, 64) for x in range(224, 288) ])   # padding
-tmr_regs.extend(imsr_regs)
-tmr_regs.extend(inia_regs)
+tmr_regs = [('TMR%d' % x, 64) for x in range(1024)]
+
+# Some TMR registers have specific names
+tmr_regs[16] = ('TMCFG0', 64)
+
+TPRIn_OFFSET = 192
+for i in range(32):
+    tmr_regs[TPRIn_OFFSET + i] = ('TPRI%d' % i, 64)
+
+IMSRn_OFFSET = 288
+for i in range(32):
+    tmr_regs[IMSRn_OFFSET + i] = ('IMSR%d' % i, 64)
+
+INIAn_OFFSET = 320
+for i in range(32):
+    tmr_regs[INIAn_OFFSET + i] = ('INIA%d' % i, 64)
+
 
 REG_OFFSET_TMR = len(ppc_regs)
 ppc_regs64.extend(tmr_regs)
 ppc_regs32.extend(tmr_regs)
 
 
+dcr_regs = [('DCR%d' % x, 32) for x in range(1024)]
+
+# Some DCR registers have specific names
+dcr_regs[272] = ('PSCR', 32)
+dcr_regs[273] = ('PSSR', 32)
+dcr_regs[274] = ('PSHR', 32)
+dcr_regs[275] = ('PSLR', 32)
+dcr_regs[276] = ('PSCTR', 32)
+dcr_regs[277] = ('PSUHR', 32)
+dcr_regs[278] = ('PSULR', 32)
+dcr_regs[351] = ('DCACNTL', 32)
+dcr_regs[352] = ('DCACNTA', 32)
+
 REG_OFFSET_DCR = len(ppc_regs)
-ppc_regs64.extend([('dcr%d' % x, 64) for x in range(1024)])
-ppc_regs32.extend([('dcr%d' % x, 64) for x in range(1024)])
+ppc_regs64.extend(dcr_regs)
+ppc_regs32.extend(dcr_regs)
 
 
-pmr_regs = []
-pmr_regs.extend([('upmc%d' % x, 32) for x in range(16)])
-pmr_regs.extend([('pmc%d' % x, 32) for x in range(16)])
-pmr_regs.extend([('foo%d' % x, 32) for x in range(96)])
-pmr_regs.extend([('upmlca%d' % x, 32) for x in range(16)])
-pmr_regs.extend([('pmlca%d' % x, 32) for x in range(16)])
-pmr_regs.extend([('bar%d' % x, 32) for x in range(96)])
-pmr_regs.extend([('upmlcb%d' % x, 32) for x in range(16)])
-pmr_regs.extend([('pmlcb%d' % x, 32) for x in range(16)])
-pmr_regs.extend([('baz%d' % x, 32) for x in range(96)])
-pmr_regs.append(('upmgc0', 64))
-pmr_regs.extend([('fuq%d' % x, 32) for x in range(15)])
-pmr_regs.append(('pmgc0', 64))
+pmr_regs = [('PMR%d' % x, 32) for x in range(1024)]
+
+# Some PMR registers have specific names
+
+UPMCn_OFFSET = 0
+for i in range(16):
+    pmr_regs[UPMCn_OFFSET + i] = ('UPMC%d' % i, 32)
+
+PMCn_OFFSET = 16
+for i in range(16):
+    pmr_regs[PMCn_OFFSET + i] = ('PMC%d' % i, 32)
+
+UPMLCAn_OFFSET = 128
+for i in range(16):
+    pmr_regs[UPMLCAn_OFFSET + i] = ('UPMLCA%d' % i, 32)
+
+PMLCAn_OFFSET = 144
+for i in range(16):
+    pmr_regs[PMLCAn_OFFSET + i] = ('PMLCA%d' % i, 32)
+
+UPMLCBn_OFFSET = 256
+for i in range(16):
+    pmr_regs[UPMLCBn_OFFSET + i] = ('UPMLCB%d' % i, 32)
+
+PMLCBn_OFFSET = 272
+for i in range(16):
+    pmr_regs[PMLCBn_OFFSET + i] = ('PMLCB%d' % i, 32)
+
+pmr_regs[384] = ('UPMGC0', 32)
+pmr_regs[400] = ('PMGC0', 32)
 
 REG_OFFSET_PMR = len(ppc_regs)
 ppc_regs64.extend(pmr_regs)
 ppc_regs32.extend(pmr_regs)
 
-#REG_OFFSET_TBR = len(ppc_regs)
+
+# The TBRs are just SPRs
 REG_OFFSET_TBR = REG_OFFSET_SPR
 
-# dynamically create REG_EAX and the like in our module
+
 l = locals()
 e_reg.addLocalEnums(l, ppc_regs)
 
@@ -186,14 +242,21 @@ ppc_meta64 = [
 ]
 
 vec_meta = [('v%d' % d, REG_OFFSET_VECTOR + d, 0, 128) for d in range(32)]
-spe_meta = [('ev%d' % d, d, 0, 64) for d in range(32)]
-spe_meta.extend([('ev%dh', d, 32, 32) for d in range(32)])   # upper half
+
+# VRSAVE is an alias for USPRG0
+vec_meta.append(('VRSAVE', REG_USPRG0, 0, 64))
 
 ppc_meta32.extend(vec_meta)
 ppc_meta64.extend(vec_meta)
 
+
+# Upper and Lower half aliases for debugging
+spe_meta = [('ev%d' % d, d, 0, 64) for d in range(32)]
+spe_meta.extend([('ev%dh', d, 32, 32) for d in range(32)])   # upper half
+
 ppc_meta32.extend(spe_meta)
 ppc_meta64.extend(spe_meta)
+
 
 statmetas = [
         ('CR0_LT', REG_CR, 63-32, 1, 'Less Than Flag'),
@@ -317,12 +380,6 @@ statmetas = [
         ('SPEFSCR_FRMC', REG_SPEFSCR, 63-62, 2, 'Embedded floating-point rounding mode control'),
 ]
 
-def getCrFields(regval):
-    ret = []
-    for name,regval,shift,bits,desc in statmetas:
-        ret.append( (name, regval >> shift & 1) )
-    return ret
-
 e_reg.addLocalStatusMetas(l, ppc_meta64, statmetas, 'EFLAGS')
 e_reg.addLocalMetas(l, ppc_meta64)
 
@@ -341,40 +398,59 @@ class Ppc64RegisterContext(e_reg.RegisterContext):
         self.setRegisterIndexes(REG_PC, REG_SP, srindex=REG_CR)
 
 
+# Special groups of PPC register names
 
-regs_general = []
-regs_general.extend([reg for reg, size in gprs64])
-#regs_general.extend([reg for reg, size in floats])
-#regs_general.extend([reg for reg, size in vectors])
-regs_general.append('lr')
-regs_general.append('xer')
-regs_general.append('ctr')
-regs_general.extend([reg for reg, size in sysregs])
+# General = GPRs
+regs_general = [name for name, _ in gprs64]
 
-regs_core = []
-regs_core.extend([reg for reg, size in gprs64])
-regs_core.append('pc')
-regs_core.append('msr')
-regs_core.append('cr')
-regs_core.append('lr')
-regs_core.append('ctr')
-regs_core.append('xer')
+# Core = GPRs + system registers + a few common SPRs
+regs_core = [name for name, _ in gprs64]
+regs_core.extend(name for name, _ in sysregs)
+regs_core.extend((
+    'lr',
+    'xer',
+    'ctr',
+))
 
-regs_fpu = ['f%d' %x for x in range(32)]
-regs_fpu.append('fpscr')
+regs_fpu = [name.lower() for name, _ in float_regs]
 
-regs_altivec = ['vr%d' %x for x in range(64)]
-regs_altivec.append('vscr')
+regs_altivec = [name.lower() for name, _ in vector_regs]
 regs_altivec.append('vrsave')
 
-regs_spe = ['ev%dh' %x for x in range(32)]
-regs_spe.append('acc')
-regs_spe.append('spefscr')
+regs_spe = [name for name, _ in spe_regs]
+regs_spe.extend(name for name, _, _, _ in spe_meta)
+regs_spe.extend((
+    'acc',
+    'spefscr',
+))
 
-regs_spr = [name for idx, (name, desc, bitsize) in spr.sprs.items()]
+regs_spr = [name for _, (name, _, _) in spr.sprs.items()]
+
+# TB registers are a specific subset of SPRs
+regs_timebase = [
+    'tcr',
+    'tsr',
+    'tb',
+    'tbu',
+    'dec',
+    'decar',
+    'atb',
+]
+
+# TMR registers are the named TMR registers and a specific subset of SPRs
+regs_thread = [name.lower() for name, _ in tmr_regs if not name.startswith('TMR')]
+regs_thread.extend((
+    'tensr',
+    'tens',
+    'tenc',
+    'ppr32',
+    'tir',
+))
+
+regs_device = [name.lower() for name, _ in dcr_regs if not name.startswith('DCR')]
+
+regs_performance = [name.lower() for name, _ in pmr_regs if not name.startswith('PMR')]
 
 
 rctx32 = Ppc32RegisterContext()
 rctx64 = Ppc64RegisterContext()
-
-
