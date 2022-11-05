afd4a47325160701bcb03c721988bc285a9f660b
Erin Cornelius <a.aaron.cornelius@gmail.com>
2022-11-04 20:07:18 -0400

    some last riscv ABI cleanup

--- vivisect/parsers/elf.py
+++ vivisect/parsers/elf.py
@@ -221,8 +221,11 @@ archcalls = {
     'arm': 'armcall',
     'thumb': 'armcall',
     'thumb16': 'armcall',
-    'rv32': 'riscvcall',
-    'rv64': 'riscvcall',
+
+    # Assume the default calling convention names for the standard RISC-V 32 and
+    # 64-bit architectures
+    'rv32': 'ilp32d',
+    'rv64': 'lp64d',
 }
 
 def getAddBaseAddr(elf, baseaddr=None):
