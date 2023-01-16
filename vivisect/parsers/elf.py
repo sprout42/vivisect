d7d5f00dc4124caa8df4aaa56adce59931b3495b
Erin Cornelius <a.aaron.cornelius@gmail.com>
2023-01-16 11:14:21 -0500

    handling inconsistent section sizes for RISC-V elf files

--- vivisect/parsers/elf.py
+++ vivisect/parsers/elf.py
@@ -182,6 +182,12 @@ def makeRelocTable(vw, va, maxva, baseoff, addend=False):
 def makeFunctionTable(elf, vw, tbladdr, size, tblname, funcs, ptrs, baseoff=0):
     logger.debug('makeFunctionTable(tbladdr=0x%x, size=0x%x, tblname=%r,  baseoff=0x%x)', tbladdr, size, tblname, baseoff)
     psize = vw.getPointerSize()
+
+    # If the provided buffer is shorter than the standard pointer size, use the
+    # buffer length
+    if size < psize:
+        psize = size
+
     fmtgrps = e_bits.fmt_chars[vw.getEndian()]
     pfmt = fmtgrps[psize]
     secbytes = elf.readAtRva(tbladdr, size)
@@ -397,8 +403,8 @@ def loadElfIntoWorkspace(vw, elf, filename=None, baseaddr=None):
 
     f_preinita = elf.dyns.get(Elf.DT_PREINIT_ARRAY)
     if f_preinita is not None:
-        f_preinitasz = elf.dyns.get(Elf.DT_PREINIT_ARRAY)
-        makeFunctionTable(elf, vw, f_preinita, f_preinitasz, 'preinit_array', new_functions, new_pointers, baseoff)
+        f_preinitasz = elf.dyns.get(Elf.DT_PREINIT_ARRAYSZ)
+        makeFunctionTable(elf, vw, f_preinita, f_preinitasz, 'preinit_array', new_functions, new_pointers, baseaddr, addbase)
 
     # dynamic table
     phdr = elf.getDynPHdr()     # this is the Program Header which points at the DYNAMICS table, not the other way around.
