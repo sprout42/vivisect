0358f692daa02b3b292bb72f730835f79085e000
Erin <493589+sprout42@users.noreply.github.com>
2023-05-01 13:43:00 -0400

    some faster srec parsing formats, but it breaks vstruct functionality

--- vstruct/defs/srec.py
+++ vstruct/defs/srec.py
@@ -26,90 +26,73 @@ S9_STARTADDR = 9
 SCOMMENT = 10
 
 
-class SRecComment(vstruct.VStruct):
+SREC_ADDR_RANGES = {
+    S0_HEADER: range(1, 3),
+    S1_DATA: range(1, 3),
+    S2_DATA: range(1, 4),
+    S3_DATA: range(1, 5),
+    S5_COUNT: range(1, 3),
+    S6_COUNT: range(1, 4),
+    S7_STARTADDR: range(1, 5),
+    S8_STARTADDR: range(1, 4),
+    S9_STARTADDR: range(1, 3),
+}
+
+SREC_ADDR_SHIFTS = {
+    S0_HEADER: (8, 0),
+    S1_DATA: (8, 0),
+    S2_DATA: (16, 8, 0),
+    S3_DATA: (24, 16, 8, 0),
+    S5_COUNT: (8, 0),
+    S6_COUNT: (16, 8, 0),
+    S7_STARTADDR: (24, 16, 8, 0),
+    S8_STARTADDR: (16, 8, 0),
+    S9_STARTADDR: (8, 0),
+}
+
+def get_srec_addr(data, code):
+    return sum(data[i] << s for s, i in zip(SREC_ADDR_SHIFTS[code], SREC_ADDR_RANGES[code]))
+
+class SRecFile:
+    '''
+    One or more chunks of binary data
+    '''
     def __init__(self):
-        vstruct.VStruct.__init__(self)
-        self.data           = v_zstr()
-        self.recordtype     = SCOMMENT
+        self.chunks         = []
+        self.entrypoints    = []
 
-class SRecChunk(vstruct.VStruct):
-
-    def __init__(self):
-        vstruct.VStruct.__init__(self)
-        self.startcode      = v_str(1)
-        self.recordtype     = v_str(1)
-        self.bytecount      = v_str(2)
-        self.address        = v_str(4)
-        self.data           = v_str(0)
-        self.csum           = v_str(2)
-
-    def pcb_bytecount(self):
-        dsize = int(self.bytecount, 16) 
-        dsize -= len(self.vsGetField('address')) // 2
-        dsize -= 1  # csum
-        self.vsGetField('data').vsSetLength( 2 * dsize )
-
-    def pcb_recordtype(self):
-        rtype = int(self.recordtype, 16)
-        if rtype in (S1_DATA, S9_STARTADDR):
-            self.vsGetField('address').vsSetLength(4)
-        elif rtype in (S2_DATA, S8_STARTADDR):
-            self.vsGetField('address').vsSetLength(6)
-        elif rtype in (S3_DATA, S7_STARTADDR):
-            self.vsGetField('address').vsSetLength(8)
-
-    def getAddress(self):
+    def vsParse(self, data, offset=0):
         '''
-        Parse the address field and return the int type.
+        Custom parsing for the SRecord type
         '''
-        return int( self.address, 16 )
+        for line in data[offset:].splitlines(keepends=True):
+            # We use keepends so we can know exactly how many bytes have been
+            # parsed
+            offset += len(line)
 
-    def getData(self):
-        '''
-        Return the binary data payload for this chunk.
-        '''
-        return binascii.unhexlify(self.data)
+            line = line.strip()
+            # out of spec, but common: treat lines which don't begin with 'S' as
+            # comments
+            if line and line[0] == 'S':
+                rtype = int(line[1])
+                line_bytes = bytes.fromhex(line[2:])
+                addr = get_srec_addr(line_bytes, rtype)
 
-class SRecFile(vstruct.VArray):
-
-    def vsParse(self, bytes, offset=0):
-
-        lines = bytes[offset:].splitlines()
-
-        for line in lines:
-            offset += 1 # there is an eaten newline for each
-            if not line:
-                continue
-
-            # out of spec, but common: treat lines which don't begin with "S" as comments
-            if line.startswith(b"S"):
-                c = SRecChunk()
-            else:
-                c = SRecComment()
-            try:
-                c.vsParse( line )
-                offset += len( c )
-
-                self.vsAddElement( c )
-            except Exception as e:
-                logger.warning("Exception parsing: %r", e)
+                if rtype in (S1_DATA, S2_DATA, S3_DATA):
+                    data_start = SREC_ADDR_RANGES[rtype].stop
+                    self.chunks.append((addr, line_bytes[data_start:-1]))
+                elif rtype in (S7_STARTADDR, S8_STARTADDR, S9_STARTADDR):
+                    self.entrypoints.append(addr)
+                    break
 
         return offset
 
     def vsEmit(self, fast=False):
-        """
-        Get back the byte sequence associated with this structure.
-        """
-        if fast:
-            if self._vs_fastfields is None:
-                self._vsInitFastFields()
-            ffvals = [ ff.vsGetValue() for ff in self._vs_fastfields ]
-            return struct.pack(self._vs_fastfmt, *ffvals)
-
-        ret = b''
-        for fname, fobj in self.vsGetFields():
-            ret += fobj.vsEmit() + b'\r\n'
-        return ret.decode('utf-8')
+        '''
+        Get back the byte sequence associated with this structure. For the SREC
+        file just return all of the data chunks concatenated together.
+        '''
+        return b''.join(c for _, c in sorted(self.chunks))
 
     def getEntryPoints(self):
         '''
@@ -117,58 +100,30 @@ class SRecFile(vstruct.VArray):
         return it.  Returns None if the 32bit entry point extension
         is not present.
         '''
-        evas = []
-        for fname, chunk in self:
-            ctype = int( chunk.recordtype, 16 )
-            if ctype in (S7_STARTADDR, S8_STARTADDR, S9_STARTADDR):
-                evas.append(int(chunk.address, 16))
-
-        return evas
+        return self.entrypoints
 
     def getMemoryMaps(self):
         '''
         Retrieve a set of memory maps defined by this hex file.
 
         Memory maps are returned as a list of
-        ( va, perms, fname, bytes ) tuples.
-        '''
-        # Get all the binary parts....
-        memparts = []
+        ( va, perms, fname, data ) tuples.
+         '''
+        memparts = {}
+        offset_to_base = {}
 
-        for fname, chunk in self:
-            ctype = int( chunk.recordtype, 16 )
-            #print fname, chunk.tree(), ctype
-
-            if ctype in (S1_DATA, S2_DATA, S3_DATA):
-                addr = chunk.getAddress()
-                memparts.append( (addr, chunk.getData()) )
-                continue
-
-            elif ctype in (S7_STARTADDR, S8_STARTADDR, S9_STARTADDR):
-                continue
-
-            elif ctype in (S5_COUNT, S6_COUNT):
-                continue
-
-            elif ctype == S0_HEADER:
-                logger.warning("HEADER: %r", chunk.data)
-                continue
-
-            elif ctype == SCOMMENT:
-                logger.warning("Comment: %r", chunk.data)
-                continue
-
-            raise Exception('Unhandled SREC chunk: %s' % chunk.recordtype)
-
-        memparts.sort()
+        for offset, data in sorted(self.chunks):
+            if offset not in offset_to_base:
+                memparts[offset] = bytearray(data)
+                offset_to_base[offset+len(data)] = offset
+            else:
+                base = offset_to_base[offset]
+                memparts[base] += data
+                del offset_to_base[offset]
+                offset_to_base[base+len(memparts[base])] = base
 
         maps = []
-        for addr, bytes in memparts:
-            # is this the next contiguous chunk?
-            if maps and addr == ( maps[-1][0] + len(maps[-1][3]) ):
-                maps[-1][3] += bytes
-            else:
-                maps.append( [ addr, e_const.MM_RWX, '', bytes ] )
-
+        for addr, data in sorted(memparts.items()):
+            maps.append( [ addr, e_const.MM_RWX, '', bytes(data) ] )
         return maps
 
